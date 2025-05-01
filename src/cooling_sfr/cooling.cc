b17ab57b78fa1042fffd94bdefc82855bc7d4f4a
Ezra Huscher <waterspring@gmail.com>
2025-04-30 22:43:15 -0600

    asdf

--- src/cooling_sfr/cooling.cc
+++ src/cooling_sfr/cooling.cc
@@ -9,15 +9,24 @@
 #include <string.h>
 #include <cmath>
 
-#include "../cooling_sfr/cooling.h"
 #include "../data/allvars.h"
 #include "../data/dtypes.h"
+#include "../data/intposconvert.h"
 #include "../data/mymalloc.h"
+#include "../domain/domain.h"
+#include "../gravity/ewald.h"
+#include "../gravity/grav_forcetest.h"
+#include "../gravtree/gravtree.h"
 #include "../logs/logs.h"
 #include "../logs/timer.h"
+#include "../main/main.h"
+#include "../main/simulation.h"
+#include "../mpi_utils/generic_comm.h"
+#include "../mpi_utils/mpi_utils.h"
+#include "../ngbtree/ngbtree.h"
+#include "../pm/pm.h"
 #include "../system/system.h"
 #include "../time_integration/timestep.h"
-#include "../data/constants.h"
 
 #define RHO_TABLE_SIZE 200
 #define TEMP_TABLE_SIZE 200
