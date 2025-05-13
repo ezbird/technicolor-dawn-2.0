e8781a0c0f948c4d4f6a1e3c28e32272de64b0fe
Ezra Huscher <waterspring@gmail.com>
2025-05-13 08:02:06 -0600

    asdf

--- src/main/init.cc
+++ src/main/init.cc
@@ -22,6 +22,7 @@
 #include <cstring>
 
 #include "../cooling_sfr/cooling.h"
+#include "../cooling_sfr/dust.h"
 #include "../data/allvars.h"
 #include "../data/dtypes.h"
 #include "../data/mymalloc.h"
